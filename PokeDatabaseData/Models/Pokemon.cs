59136916ade39faf5be2701c217f3e2eb04033ea
George Okello <okellogeorge40@yahoo.com>
2018-01-12 22:45:44 -0600

    Add pokemon model

---
+++ PokeDatabaseData/Models/Pokemon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokeDatabaseData.Models
+{
+    public class Pokemon
+    {
+        public int pokId { get; set; }
+        public string pokName { get; set; }
+        public int SpeciesId { get; set; }
+        public double height { get; set; }
+        public decimal Weight { get; set; }
+        public int baseExp { get; set; }
+    }
+}
---
+++ PokeDatabaseData/PokemonContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace PokeDatabaseData
+{
+    public class PokemonContext : DbContext
+    {
+        public PokemonContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<PokemonContext> Pokemons { get; set; }
+    }
+}
